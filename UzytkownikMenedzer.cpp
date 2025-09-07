ceb1fb06632bf0329011f60535d14f349a2b81f4
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-09-07 12:18:34 +0200

    Refactor

--- UzytkownikMenedzer.cpp
+++ UzytkownikMenedzer.cpp
@@ -64,5 +64,5 @@ void UzytkownikMenedzer::wypiszWszystkichUzytkownikow()
 
 void UzytkownikMenedzer::wczytajUzytkownikowZPliku()
 {
-    plikZUzytkownikami.wczytajUzytkownikowZPliku(uzytkownicy);
+    uzytkownicy = plikZUzytkownikami.wczytajUzytkownikowZPliku();
 }

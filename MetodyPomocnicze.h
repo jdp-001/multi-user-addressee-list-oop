69ee06e3e1a779aca9e388fbc095b293a37a9431
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-10-12 19:00:13 +0200

    Add wczytajZnak and wczytajLiczbeCalkowita methods in MetodyPomocnicze class

--- MetodyPomocnicze.h
+++ MetodyPomocnicze.h
@@ -16,6 +16,8 @@ public:
     static string pobierzLiczbe(string tekst, int pozycjaZnaku);
     static int konwersjaStringNaInt(string liczba);
     static string zamienPierwszaLitereNaDuzaAPozostaleNaMale(string tekst);
+    static char wczytajZnak();
+    static int wczytajLiczbeCalkowita();
 };
 
 #endif // METODYPOMOCNICZE_H

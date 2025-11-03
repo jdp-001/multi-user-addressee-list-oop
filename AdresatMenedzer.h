e2e6786730c18b5c18982423f16870d0cba2ae62
Jaroslaw Dolinski <jaroslaw.dolinski.programista@gmail.com>
2025-11-03 18:57:49 +0100

    Cleanup comments in AdresatMenedzer class header

--- AdresatMenedzer.h
+++ AdresatMenedzer.h
@@ -4,12 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
-#include <fstream> // Do usuniecia?
 #include <sstream>
-#include <algorithm> // Do usuniecia?
 
 #include "Adresat.h"
-#include "MetodyPomocnicze.h" // Do usuniecia?
 #include "PlikZAdresatami.h"
 
 using namespace std;
@@ -17,42 +14,33 @@ using namespace std;
 class AdresatMenedzer
 {
     const int ID_ZALOGOWANEGO_UZYTKOWNIKA;
-    //int idOstatniegoAdresata; // Do usuniecia?
     int idUsunietegoAdresata;
 
     vector <Adresat> adresaci;
     PlikZAdresatami plikZAdresatami;
 
-    //int podajIdOstatniegoAdresataPoUsunieciuWybranegoAdresata();
     void wyswietlDaneAdresata(Adresat adresat);
     Adresat podajDaneNowegoAdresata();
-    //string zamienPierwszaLitereNaDuzaAPozostaleNaMale(string tekst); // Do usuniecia?
 
 public:
     AdresatMenedzer(string nazwaPlikuZAdresatami, int idZalogowanegoUzytkownika)
     : plikZAdresatami(nazwaPlikuZAdresatami), ID_ZALOGOWANEGO_UZYTKOWNIKA(idZalogowanegoUzytkownika)
     {
         adresaci = plikZAdresatami.wczytajAdresatowZalogowanegoUzytkownikaZPliku(ID_ZALOGOWANEGO_UZYTKOWNIKA);
-        //idOstatniegoAdresata = plikZAdresatami.pobierzIdOstatniegoAdresata(); // <--- to jest kluczowe, ale tego nie widze u Artura do minuty 21:00 !!!!!
     };
 
     // Getters
-    int pobierzIdZalogowanegoUzytkownika(); // tego nie ma u Artura!
-    // 29.10.2025
+    int pobierzIdZalogowanegoUzytkownika();
     int pobierzIdUsunietegoAdresata();
 
     // Setters
-    void ustawIdZalogowanegoUzytkownika(int idPrzekazywane); // tego nie ma u Artura!
 
-    void pobierzAdresatowZalogowanegoUzytkownikaZPliku(); // tego nie mna u Artura!
+    void pobierzAdresatowZalogowanegoUzytkownikaZPliku();
     void dodajAdresata();
     void wypiszWszystkichAdresatow(); // U Artura inna nazwa - wyswietlWszystkichAdresatow()
-    // 25.10.2025
     void wyszukajAdresatowPoImieniu();
     void wyszukajAdresatowPoNazwisku();
-    // 28.10.2025
     void usunAdresata();
-    // 01.11.2025
     void edytujAdresata();
     char wybierzOpcjeZMenuEdycja();
     void zaktualizujDaneWybranegoAdresata(Adresat adresat);
